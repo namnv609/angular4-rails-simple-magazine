b844f96a5899d6b4819ef387096df28106c5616b
NamNV609 <namnv609@gmail.com>
2018-04-30 13:06:02 +0700

    Add site layout

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { RouterModule } from "@angular/router";
 import { APP_ROUTINGS } from "./app.routings";
 
 import { AppComponent } from './app.component';
+import { SiteLayoutComponent } from './_layout/site-layout/site-layout.component';
+import { SiteHeaderComponent } from './_layout/site-header/site-header.component';
+import { SiteFooterComponent } from './_layout/site-footer/site-footer.component';
+import { SiteHomeComponent } from './components/site/site-home/site-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteLayoutComponent,
+    SiteHeaderComponent,
+    SiteFooterComponent,
+    SiteHomeComponent
   ],
   imports: [
     BrowserModule,
